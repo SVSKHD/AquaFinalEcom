64320ce5ecb0be2230bd0812099ff50436b3c068
hithesh svsk <62789585+SVSKHD@users.noreply.github.com>
2022-12-21 23:32:28 +0530

    new ecom

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './registerServiceWorker'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+import store from "./store";
+import { Quasar } from "quasar";
+import quasarUserOptions from "./quasar-user-options";
 
-createApp(App).use(store).use(router).mount('#app')
+const AquaEcom = createApp(App);
+
+AquaEcom.use(Quasar, quasarUserOptions).use(store).use(router).mount("#app");
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,31 @@
 import { createRouter, createWebHistory } from 'vue-router'
+//layout
+import Layout from "../layout/Layout.vue"
+//pages
 import HomeView from '../views/HomeView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'Layout',
+    component: Layout,
+    children:[
+      {
+        path: '/',
+        name: 'home',
+        component: HomeView,
+      },
+      {
+        path: '/about',
+        name: 'about',
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+      }
+    ]
   },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+  
 ]
 
 const router = createRouter({
